2145616e1546f8d8b33431a16777c0be8058a9e5
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-08-23 15:30:18 +0300

    named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,26 +8,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                body1: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 51),
-                ),
-                body2: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 51),
-                ),
-                title: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-      home: CategoriesScreen(),
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            accentColor: Colors.amber,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  body1: TextStyle(
+                    color: Color.fromRGBO(20, 51, 51, 51),
+                  ),
+                  body2: TextStyle(
+                    color: Color.fromRGBO(20, 51, 51, 51),
+                  ),
+                  title: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'RobotoCondensed',
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+        home: CategoriesScreen(),
+        routes: {
+          '/category-meals': (ctx) => CategoryMealsScreen(),
+        });
   }
 }
